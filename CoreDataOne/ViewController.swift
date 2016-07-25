1099a70e26e68de069bcc382c7f42fdd9372add1
D.A Jalloh <info@codewithjalloh.com>
2016-07-25 19:35:07 +0100

    viewDidLoad added

--- CoreDataOne/ViewController.swift
+++ CoreDataOne/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    var results: NSArray?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        // INSERT 
+        let celldata = NSEntityDescription.insertNewObjectForEntityForName("cell", inManagedObjectContext: context)
+        celldata.setValue("Yoda Tux", forKey: "title")
+        celldata.setValue("Science Fiction", forKey: "subtitle")
+        celldata.setValue("yodaTux.png", forKey: "image")
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error!")
+        }
+        
+        let request = NSFetchRequest(entityName: "cell")
+        request.returnsObjectsAsFaults = false
+        results = try? context.executeFetchRequest(request)
+        
+        if (results!.count>0) {
+            for res in results! {
+                print(res)
+            }
+        }
+    
+        
     }
 
     override func didReceiveMemoryWarning() {
